99b99e1f47b0d956559bf31276ae92ae053f5b3d
Mateus Ribeiro <mateusrfon@gmail.com>
2021-06-08 14:22:03 -0300

    correçoes e funcionalidades extras

--- todo.js
+++ todo.js
@@ -1,33 +1,74 @@
 const readlineSync = require('readline-sync');
+const fs = require('fs');
+let todoList = [];
 
-const todoList = [];
-const funcList = ["add", "list", "check", "remove"];
+if (fs.existsSync('./todoList.txt')) {
+    todoList = JSON.parse(fs.readFileSync('./todoList.txt'));
+}
+
+const funcList = ["add", "list", "check", "remove", "pomodoro"];
 
 function start() {
     const index = readlineSync.keyInSelect(funcList);
     if (funcList[index] === "add") {
         const newTodo = readlineSync.question('Qual a todo a ser adicionada? ');
-        todoList.push(`[ ] ${newTodo}`);
+        todoList.push(`🔴 ${newTodo}`);
+        fs.writeFileSync('./todoList.txt', JSON.stringify(todoList));
         start();
     } else if (funcList[index] === "list") {
-        console.log('====================');
-        todoList.forEach(e => console.log(e));
-        console.log('====================');
+        console.log('========================');
+        todoList.length > 0 ? todoList.forEach(e => console.log(e)) : console.log('Nenhuma todo registrada.');
+        console.log('========================');
         start();
     } else if (funcList[index] === "check") {
-        const todoToCheck = readlineSync.keyInSelect(todoList, 'Qual todo deseja dar check-uncheck? ');
-        if (todoList[todoToCheck].length > 0) {
-            if (todoList[todoToCheck][1] === " ") {
-                todoList[todoToCheck] = todoList[todoToCheck].replace(' ', 'X');
-            } else {
-                todoList[todoToCheck] = todoList[todoToCheck].replace('X', ' ');
+        if (todoList.length > 0) {
+            const todoToCheck = readlineSync.keyInSelect(todoList, 'Qual todo deseja dar check-uncheck? ');
+            if (todoToCheck > -1) {
+                if (todoList[todoToCheck].includes('🔴')) {
+                    todoList[todoToCheck] = todoList[todoToCheck].replace('🔴', '🟢');
+                    fs.writeFileSync('./todoList.txt', JSON.stringify(todoList));
+                } else {
+                    todoList[todoToCheck] = todoList[todoToCheck].replace('🟢', '🔴');
+                    fs.writeFileSync('./todoList.txt', JSON.stringify(todoList));
+                }
             }
         }
+        console.log('========================');
+        todoList.length > 0 ? todoList.forEach(e => console.log(e)) : console.log('Nenhuma todo registrada.');
+        console.log('========================');
         start();
     } else if (funcList[index] === "remove") {
-        const removeTodo = readlineSync.keyInSelect(todoList,'Qual a todo a ser removida? ');
-        todoList.splice(removeTodo, 1);
+        if (todoList.length > 0) {
+            const removeTodo = readlineSync.keyInSelect(todoList,'Qual a todo a ser removida? ');
+            todoList.splice(removeTodo, 1);
+            fs.writeFileSync('./todoList.txt', JSON.stringify(todoList));
+        }
+        console.log('========================');
+        todoList.length > 0 ? todoList.forEach(e => console.log(e)) : console.log('Nenhuma todo registrada.');
+        console.log('========================');
         start();
+    } else if (funcList[index] === "pomodoro") {
+        if (todoList.length > 0) {
+            const pomodoro = readlineSync.keyInSelect(todoList,'Qual a todo a fazer pomodoro? ');
+            console.log(`Pomodoro de "${todoList[pomodoro]}" setado!`);
+            setTimeout(() => {
+                if (todoList[pomodoro].includes("🍅")) {
+                    todoList[pomodoro] = todoList[pomodoro] + "🍅";
+                } else {
+                    todoList[pomodoro] = todoList[pomodoro] + " " + "🍅";
+                }            
+                fs.writeFileSync('./todoList.txt', JSON.stringify(todoList));
+                console.log('========================');
+                todoList.length > 0 ? todoList.forEach(e => console.log(e)) : console.log('Nenhuma todo registrada.');
+                console.log('========================');
+                start();
+            }, 2000);
+        } else {
+            console.log('========================');
+            todoList.length > 0 ? todoList.forEach(e => console.log(e)) : console.log('Nenhuma todo registrada.');
+            console.log('========================');
+            start();
+        }
     }
 }
 
